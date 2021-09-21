146fb7393135dbd251b91bbab0cd1c174ad76496
Garrachonr <garrachonr@outlook.com>
2021-09-21 10:21:19 +0200

    Initial Commit

---
+++ Shared/Quiz1App.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz1App.swift
+//  Shared
+//
+//  Created by Alfredo Garrachon Ruiz on 21/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Quiz1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
